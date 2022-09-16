f8359439ddab7c6a489f0a9c4395a29142d43388
osvaldo <g.osvaldo96@gmail.com>
2022-09-16 14:38:42 -0600

    added maintenance and notes classes to modules

--- backend/torq/models.py
+++ backend/torq/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 # Create your models here.
 
@@ -16,9 +17,21 @@ class Car(models.Model):
     year = models.IntegerField()
     make = models.CharField(max_length=100)
     model = models.CharField(max_length=100)
-    mileage = models.IntegerField()
-    
-   
     
     def __str__(self):
         return f"{self.year} {self.make} {self.model}"
+    
+class Maintenance(models.Model):
+    mileage = models.PositiveIntegerField()
+    brakes = models.BooleanField(default=False)
+    tires = models.BooleanField(default=False)
+    engineOil = models.PositiveIntegerField()
+    airFilter = models.BooleanField(default=False)
+    timestamp = models.DateField(default=now)
+    car = models.ForeignKey('Car', on_delete=models.CASCADE, related_name='maintenances')
+    
+class Notes(models.Model):
+    note = models.TextField()
+    car = models.ForeignKey('Car', on_delete=models.CASCADE, related_name='notes')
+    timestamp = models.DateField(default=now)
+    
